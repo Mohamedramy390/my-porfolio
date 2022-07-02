5b942f841b039dc0fab9231a2b8a0bca9d476fb2
Mohamedramy390 <104658514+Mohamedramy390@users.noreply.github.com>
2022-07-02 18:02:09 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -21,5 +21,55 @@ window.onscroll= function(){
 $(function(){
   $('#contact-form').validate();
 })
-
 // Example starter JavaScript for disabling form submissions if there are invalid fields
+
+text = $(".animation-text").position().top;
+about = $(".about-animation").position().top;
+btn = $(".about .about-btn a").position().top;
+img = $(".info-img").position().top;
+circle = $(".circle").position().top;
+card = $(".card").position().top;
+contactinfo = $(".contact-info").position().top;
+contactform = $(".contact-form").position().top;
+smtitle = $(".sm-title").position().top;
+title = $(".heading-text h1").position().top;
+icons = $("header .heading .social-icons li").position().top;
+
+$(function() {
+    $('.sm-title').addClass('animation');
+    $(".heading-text h1").addClass("animation"); 
+    $(".header .heading .social-icons li").addClass("animation");
+  });
+
+$(window).scroll(function() {
+   
+    if(text>0 && $(window).scrollTop() + 500 >= text) {              
+        $(".animation-text").addClass("animation");
+    } 
+    if(img>0 && $(window).scrollTop() + 700 >= img) {              
+        $(".info-img").addClass("animation"); 
+    }
+    if(about>0 && $(window).scrollTop() + 430 >= about ) {              
+        $(".about-animation").addClass("animation");
+        $(".about .about-btn a").addClass("animation");
+    } 
+    if(circle>0 && $(window).scrollTop() + 500 >= circle ) {              
+        $(".circle").addClass("animation");
+    }
+    if(card>0 && $(window).scrollTop() + 300 >= card ) {              
+        $(".card").addClass("animation");
+    }
+    if(contactinfo>0 && $(window).scrollTop() + 430 >= contactinfo ) {              
+        $(".contact-info").addClass("animation");
+        $(".contact-form").addClass("animation");
+    }
+});
+
+const load = document.querySelector('.loader');
+
+window.addEventListener('load', function(){
+    load.style.display= 'none';
+})
+
+
+
